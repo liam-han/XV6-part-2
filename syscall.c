f12a03d101f2586b028602725d904e28c3e7da01
Melvin Su <sumelvin919@gmail.com>
2018-11-26 20:16:08 +0000

    ehh

--- syscall.c
+++ syscall.c
@@ -34,7 +34,7 @@ fetchstr(uint addr, char **pp)
   char *s, *ep;
   struct proc *curproc = myproc();
 
-  if(addr >= user_top)
+  if(addr >= curproc->sz)
     return -1;
   *pp = (char*)addr;
   ep = (char*)curproc->sz;
